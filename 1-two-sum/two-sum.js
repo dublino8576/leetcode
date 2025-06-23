13ad1b274c8d05bb13ce3662f2bbb54d7a3f763d
dublino8576 <58470001+dublino8576@users.noreply.github.com>
2025-06-23 12:24:27 +0100

    Time: 1 ms (87.16%) | Memory: 54 MB (84.34%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,12 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const result = []
+    /*const result = []
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 return [i, j]
             }
         }
+    }*/
+    //initiate an empty hash map (object) where we are going to add the key value pairs of values and their respective indexes
+    const hashMap = {}
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]
+        if (complement in hashMap) {
+            //checks if key matches complement as it gets stored in map object
+            return [hashMap[complement], i]
+        }
+        hashMap[nums[i]] = i
     }
+    //return empty array if solution not found
+    return []
 };
